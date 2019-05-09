6b887475c99a2adbca683736387f0fc870220289
opuahmed09 <opuahmed000@gmail.com>
2019-05-08 20:50:39 -0400

    JavaOverview

--- src/oopConcept/MainClass.java
+++ src/oopConcept/MainClass.java
@@ -7,33 +7,28 @@ public class MainClass {
 
 
         SingletonClass.getInstance().calendar();
+        RegularClass rc = new RegularClass();
 
+        rc.reguralMethod();
+        rc.city();
+        rc.calculator();
+        rc.math();
 
+        System.out.println("\t****************************\n\n");
 
+        MyAbstructClass rc1 = new RegularClass();
+        rc1.city();
+        rc1.calculator();
+        rc1.math();
+        rc1.regural();
 
+        EncapsulationConcept ec = new EncapsulationConcept();
 
-//        RegularClass rc = new RegularClass();
-//
-//        rc.reguralMethod();
-//        rc.city();
-//        rc.calculator();
-//        rc.math();
-//
-//        System.out.println("\t****************************\n\n");
-//
-//        MyAbstructClass rc1 = new RegularClass();
-//        rc1.city();
-//        rc1.calculator();
-//        rc1.math();
-//        rc1.regural();
+        ec.setName("BOB");
+        ec.setId(101);
+        ec.setDOB("12/03/1990");
 
-//        EncapsulationConcept ec = new EncapsulationConcept();
-//
-//        ec.setName("BOB");
-//        ec.setId(101);
-//        ec.setDOB("12/03/1990");
-//
-//        System.out.println(ec.getName() + "    " + ec.getId() + "  " + ec.getDOB());
+        System.out.println(ec.getName() + "    " + ec.getId() + "  " + ec.getDOB());
 
 
     }
